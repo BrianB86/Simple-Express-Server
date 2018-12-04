5aa8cfc5dcde48ca8d3d81681a9f4ea3fe67a40e
Brian <bbattell@gmail.com>
2018-12-04 12:02:45 -0500

    Adding JSON WEb token instead of sessions

--- routes/promoRouter.js
+++ routes/promoRouter.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Promotions = require('../models/promotions');
+const authenticate = require('../authenticate');
+
 
 const promoRouter = express.Router();
 
@@ -17,7 +19,7 @@ promoRouter.route('/')
   .catch((err) => next(err));
 })
 
-.post((req,res,next) => {
+.post(authenticate.verifyUser, (req,res,next) => {
   Promotions.create(req.body)
   .then((promotion) => {
     console.log('Promotion Created ', promotion);
@@ -28,12 +30,12 @@ promoRouter.route('/')
   .catch((err) => next(err));
 })
 
-.put((req,res,next) => {
+.put(authenticate.verifyUser, (req,res,next) => {
   res.statusCode = 403;
   res.end('PUT operation not supported on /promotions');
 })
 
-.delete((req,res,next) => {
+.delete(authenticate.verifyUser, (req,res,next) => {
   Promotions.remove({})
   .then((resp) => {
     res.StatusCode = 200;
@@ -55,12 +57,12 @@ promoRouter.route('/:promoId')
     .catch((err) => next(err));
   })
 
-.post((req,res,next) => {
+.post(authenticate.verifyUser, (req,res,next) => {
   res.statusCode = 403;
   res.end('POST operation not supported on /promotions/' + req.params.promoId);
 })
 
-.put((req,res,next) => {
+.put(authenticate.verifyUser, (req,res,next) => {
   Promotions.findByIdAndUpdate(req.params.promoId, {
     $set: req.body
   }, { new: true })
@@ -72,7 +74,7 @@ promoRouter.route('/:promoId')
   .catch((err) => next(err));
 })
 
-.delete((req,res,next) => {
+.delete(authenticate.verifyUser, (req,res,next) => {
   Promotions.findByIdAndRemove(req.params.promoId)
   .then((resp) => {
     res.StatusCode = 200;
